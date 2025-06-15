1c490985b9f321a62a6e6369aba50f19aaebad8b
Medicine3301 <acr3214@gmail.com>
2025-06-15 23:45:10 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import router from './Router.js'
+import router from './router.js'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 import * as AntDesignIconsVue from '@ant-design/icons-vue'
